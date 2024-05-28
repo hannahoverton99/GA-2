a045794933817fb19fb616020c0b27c461874512
Hannah Overton <hannahoverton99@gmail.com>
2024-05-28 12:07:46 -0500

    Task 2 through CoreCompetency complete

--- TechJobsOOAutoGraded6/CoreCompetency.cs
+++ TechJobsOOAutoGraded6/CoreCompetency.cs
@@ -3,9 +3,9 @@ namespace TechJobsOOAutoGraded6
 {
 	public class CoreCompetency
 	{
-        private int id;
+        public int id {get; set; }
         private static int nextId = 1;
-        private string value;
+        public string value {get ; set; }
 
         // TODO: Task 2: Change the fields to auto-implemented properties.
 
